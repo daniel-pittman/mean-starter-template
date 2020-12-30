37c89767d23ba88199720d16f56ac636010b520a
Daniel Pittman <daniel@pittmo.net>
2020-12-29 22:41:33 -0700

    Updates to angular routing and tsconfig to ignore dist folder

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -19,10 +19,18 @@ export function tokenGetter() {
     return localStorage.getItem('id_token');
 }
 
-const appRoutes: Routes = [{ path: '',
+const appRoutes: Routes = [
+  {
+    path: '',
     redirectTo: '/home',
     pathMatch: 'full'
-}];
+  },
+  {
+    path: '**',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
     imports: [
